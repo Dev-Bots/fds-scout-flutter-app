2e31a85196296bb9523c63c5a770b3398e94f0d1
Yonatan Awoke <it.yonatan.awoke@gmail.com>
2022-04-30 23:05:14 -0700

    the game class created

---
+++ lib/Data/models/Game/game.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: file_names
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class Game extends Equatable {
+  const Game({
+    required this.gameId,
+    required this.eventId,
+    required this.team1,
+    required this.team2,
+  });
+
+  final int? gameId;
+  final int? eventId;
+  final int? team1;
+  final int? team2;
+
+  @override
+  List<Object?> get props => [gameId, eventId, team1, team2];
+
+  factory Game.fromJson(Map<String, dynamic> json) => Game(
+        gameId: json['id'],
+        eventId: json['event'],
+        team1: json['team1'],
+        team2: json['team2'],
+
+        // club: json['club'],
+      );
+
+  @override
+  String toString() =>
+      "TestMatches { eventId: $eventId, Team $team1 vs Team $team2}";
+}
