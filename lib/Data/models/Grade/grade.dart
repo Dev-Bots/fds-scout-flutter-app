a386dc441b4f169f27ad91032b88f6e6ecdd34a0
Yonatan Awoke <it.yonatan.awoke@gmail.com>
2022-05-02 21:56:51 -0700

    score and grading class formed

---
+++ lib/Data/models/Grade/grade.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: file_names
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class Grade extends Equatable {
+  Grade(
+      {this.score,
+      this.aggregate, //needs to be removed
+      required this.eventId,
+      required this.player,
+      required this.scoutId});
+
+  var score;
+  var aggregate;
+  final int eventId;
+  final int player;
+  final int scoutId;
+
+  @override
+  List<Object?> get props => [score, eventId, player, scoutId];
+
+  factory Grade.fromJson(Map<String, dynamic> json) => Grade(
+        score: json['score'],
+        eventId: json['event'],
+        scoutId: json['scout'],
+        player: json['player'],
+        aggregate: json['aggregate'],
+      );
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['score'] = score;
+    data['event'] = eventId;
+    data['scout'] = scoutId;
+    data['player'] = player;
+    data['aggregate'] = 10;
+
+    return data;
+  }
+
+  @override
+  String toString() =>
+      "Grade { eventId: $eventId, Player $player vs score $score}";
+}
