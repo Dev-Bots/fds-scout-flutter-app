6d8a3a433fddb9d38410debf1a93522f7993f07e
Meron Mesay <it.meron.mesay@gmail.com>
2022-05-12 14:15:10 +0300

    grade state created

---
+++ lib/Bloc/GradeBloc/grade_state.dart
@@ -0,0 +1,32 @@
+part of 'grade_bloc.dart';
+
+abstract class GradeState extends Equatable {
+  const GradeState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class GradeInitial extends GradeState {}
+
+class GradeLoading extends GradeState {}
+
+class GradeUploaded extends GradeState {
+  final grade;
+
+  const GradeUploaded({required this.grade});
+  @override
+  List<Object> get props => [grade];
+}
+
+class GradeFailed extends GradeState {
+  final String error;
+
+  const GradeFailed({required this.error});
+
+  @override
+  List<Object> get props => [error];
+
+  @override
+  String toString() => 'EventFailure { error: $error }';
+}
